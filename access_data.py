00f73a3f516711b4b5ebea6fbc5e8a125f3f727c
KozhevnikovRoma <rnkozhevnikov@gmail.com>
2025-02-08 18:45:30 +0300

    Автоматическое обновление кода

---
+++ access_data.py
@@ -0,0 +1,6 @@
+# access_data.py
+ACCESS_KEY = "your_access_key_here"
+REPOSITORY_NAME = "KozhevnikovRoma/open"
+DATABASE_URL = "sqlite:///ai_coordinator.db"
+
+# Добавляем другие необходимые коды для работы с репозиторием или БД
